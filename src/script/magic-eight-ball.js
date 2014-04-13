749e12dabccd769e3ca73331c4520b7985a062c5
Josh Renner <Josh.Renner2@target.com>
2014-04-12 23:58:17 -0500

    organize folder structure and separate out styles

---
+++ src/script/magic-eight-ball.js
@@ -0,0 +1,70 @@
+(function() {
+	function random(o) {
+		return o[Math.floor(Math.random() * o.length)];
+	}
+	function shuffle(o) {
+		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	}
+	function shrink(o) {
+		return shuffle(o).splice(0, Math.floor(o.length / 2));
+	}
+	function setClass(o, c) {
+		o.className = c;
+	}
+	function addClass(o, c) {
+		o.className += " " + c;
+	}
+	function removeClass(o, c) {
+		var r = new RegExp("(?:^|\\s)" + c + "(?!\\S)", "g");
+		return o.className.replace(r, '');
+	}
+
+	Polymer("magic-eight-ball", {
+		outlook: 0,
+		prediction: "Ask me a question",
+		domReady: function () {
+			this.$.ball.addEventListener("click", this.predict.bind(this));
+		},
+		predict: function () {
+			var ball = this.$.ball,
+				pred = this.$.prediction,
+				css = ["shake", "tell"],
+				opt = random(this.getOptions());
+			setClass(ball, removeClass(ball, css[1]));
+			addClass(ball, css[0]);
+			setTimeout(function() {
+				pred.innerHTML = opt;
+				setClass(ball, removeClass(ball, css[0]));
+				addClass(ball, css[1]);
+			}, 1000);
+		},
+		getOptions: function () {
+			var options = this.children,
+				optimistic = [], pessimistic = [], list = [];
+
+			for(var n=0; n<options.length; n++){
+				var item = options[n],
+					outlook = item.getAttribute("outlook"),
+					text = item.innerHTML;
+
+				if (text === this.$.prediction.innerHTML)
+					continue;
+
+				if (outlook > 0)
+					optimistic.push(text);
+				else if (outlook < 0)
+					pessimistic.push(text);
+				else
+					list.push(text);
+			}
+
+			if (this.outlook > 0)
+				pessimistic = shrink(pessimistic);
+			if (this.outlook < 0)
+				optimistic = shrink(optimistic);
+
+			return list.concat(optimistic).concat(pessimistic);
+		}
+	})
+})();
